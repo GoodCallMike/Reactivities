6aae6a0c6876a5e6cd412fabdc72ec1482d459ca
Michael Saylor <saylormic@gmail.com>
2020-02-23 10:25:01 -0500

    End of Section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react'
 
 class App extends Component {
     state = {
@@ -11,7 +11,6 @@ class App extends Component {
     componentDidMount(): void {
         axios.get('http://localhost:5000/api/values')
             .then((response) => {
-                console.log(response);
                 this.setState({
                     values: response.data  
                 })
@@ -20,17 +19,19 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <ul>
-                        {this.state.values.map((value: any) => (
-                            <li>{value.name}</li>
-                        ))}
-                    </ul>
-                </header>
+            <div>
+                <Header as='h2'>
+                    <Icon name='users'/>
+                    <Header.Content>Reactivities</Header.Content>
+                </Header>
+                <List>
+                    {this.state.values.map((value: any) => (
+                        <List.Item key={value.id}>{value.name}</List.Item>
+                    ))}
+                </List>
             </div>
         );
+        
     }
 }
 
